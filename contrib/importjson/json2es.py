75056cc0a9ea3bd6c09584991bf629d7a397e279
boyska <piuttosto@logorroici.org>
2014-11-26 13:09:34 +0100

    Make it easier to have some data in your library

--- contrib/importjson/json2es.py
+++ contrib/importjson/json2es.py
@@ -1,6 +1,7 @@
 '''
 read json books, one per line, and put them into elasticsearch
 '''
+from __future__ import print_function
 
 import sys
 import json
@@ -12,12 +13,16 @@ if __name__ == '__main__':
     db = DB(Elasticsearch())
 
     i = 0
-    print "Loading...",
-    for line in open(sys.argv[1]):
+    print("Loading...", end='')
+    if len(sys.argv) > 1 and sys.argv[1] != '-':
+        buf = open(sys.argv[1])
+    else:
+        buf = sys.stdin
+    for line in buf:
         i += 1
         book = {'language': 'it'}
         book.update(json.loads(line))
         if i % 50:
-            print "\rLoading\t%d" % i,
+            print("\rLoading\t%d" % i, end='')
         db.add_book(doc_type='book', body=book)
-    print "\rDone\t%d" % i
+    print("\rDone\t(%d books added)" % i)

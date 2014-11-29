70e4ea047d7d4d54c2b5f2c80d6e53f9ef3d65f0
ael-code <tommy.ael@gmail.com>
2014-11-29 15:47:12 -0600

    choosed a different way to implement "compilation of translation". Now compilation of catalog messages is triggered by install, dist_egg, develop commands and translation binary are included in webant binary executable

--- setup.py
+++ setup.py
@@ -3,6 +3,38 @@ import subprocess
 
 from setuptools import setup
 
+from setuptools.command.install_lib import install_lib as _install_lib
+from setuptools.command.develop import develop as _develop
+from distutils.command.build import build as _build
+from distutils.cmd import Command
+
+class compileTranslations(Command):
+    description = 'compile message catalogs to MO files via pybabel command'
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print("\nCompiling translations:")
+        subprocess.call( os.path.join(os.path.dirname(__file__), "webant/babel/trans_compile.sh") )
+
+class build(_build):
+    sub_commands = [('compileTranslations', None)] + _build.sub_commands
+
+
+class install_lib(_install_lib):
+    def run(self):
+        self.run_command('compileTranslations')
+        _install_lib.run(self)
+
+class develop(_develop):
+     def run(self):
+        self.run_command('compileTranslations')
+        _develop.run(self)
+
 def read(fname):
     with open(os.path.join(os.path.dirname(__file__), fname)) as buf:
         return buf.read()
@@ -27,13 +59,9 @@ setup(name='libreant',
       tests_require=['nose'],
       test_suite='nose.collector',
       zip_safe=False,
-      entry_points={
-          'console_scripts': ['webant=webant.webant:main',
-                              ]
-      }
+      cmdclass={'build': build,
+                'install_lib': install_lib,
+                'develop':develop,
+                'compileTranslations': compileTranslations },
+      entry_points={'console_scripts': ['webant=webant.webant:main'] }
       )
-
-# compile translations
-print("\nCompiling translations:")
-subprocess.call( os.path.join(os.path.dirname(__file__), "webant/babel/trans_compile.sh") )
-

d4ab3e2509145c2848a1049a0de7c9d6e7cb2f11
boyska <piuttosto@logorroici.org>
2015-01-20 16:43:13 +0100

    libreant-manage db_import
    it's a "port" of the old json2es.py; now better integrated with common
    options and other tools

--- webant/manage.py
+++ webant/manage.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+import sys
 import json
 
 from flask.ext.script import Manager
@@ -16,7 +18,7 @@ def db_export(size):
     if int(size) < 0:
         size = len(db)
     for item in db.get_all_books(size=size)['hits']['hits']:
-        print json.dumps(item)
+        print(json.dumps(item))
 
 
 @manager.command
@@ -24,7 +26,7 @@ def db_item_export(item_id):
     '''exports a single item'''
     db = app.get_db()
     book = db.get_book_by_id(item_id)
-    print json.dumps(book)
+    print(json.dumps(book))
 
 
 @manager.command
@@ -33,7 +35,33 @@ def db_search(query):
     db = app.get_db()
     res = db.user_search(query)['hits']['hits']
     for item in res:
-        print json.dumps(item['_id'])
+        print(item['_id'])
+
+
+@manager.option('filename',
+                help='File to read from, or stdin if "-" is provided')
+def db_import(filename, language='it'):
+    '''imports items from a previous export'''
+    if filename == '-':
+        buf = sys.stdin
+    else:
+        buf = open(filename)
+    db = app.get_db()
+    i = 0
+    for line in buf:
+        i += 1
+        book = json.loads(line)
+        if '_source' in book:
+            book = book['_source']
+        if '_language' not in book:
+            book['_language'] = language
+
+
+        if i % 50:
+            print("\rLoading\t%d" % i, end='')
+        db.add_book(doc_type='book', body=book)
+    print("\r" + " "*30, end='')
+    print("\rDone\t(%d books added)" % i)
 
 
 def main():

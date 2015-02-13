bed8a303f03ee0464c4d336a023e6a6fcb5e36ab
boyska <piuttosto@logorroici.org>
2015-02-13 18:02:55 +0100

    FIX agherant logging
    closes #88

--- webant/agherant_standalone.py
+++ webant/agherant_standalone.py
@@ -1,21 +1,24 @@
+import logging
+
 from flask import Flask, request
 from flask_bootstrap import Bootstrap
 from flask.ext.babel import Babel
 
 import agherant
+from webant import initLoggers
 from webserver_utils import gevent_run
 import config_utils
 
 
 def create_app():
+    initLoggers()
+    conf = {'DEBUG': True,
+            'AGHERANT_DESCRIPTIONS': []}
+    conf.update(config_utils.from_envvar_file('WEBANT_SETTINGS'))
+    conf.update(config_utils.from_envvars(prefix='WEBANT_'))
+    initLoggers(logging.DEBUG if conf.get('DEBUG', False) else logging.WARNING)
     app = Flask(__name__)
-    app.config.update({
-        'AGHERANT_DESCRIPTIONS': [],
-        'DEBUG': True,
-        'SECRET_KEY': 'really insecure, please change me!'
-    })
-    app.config.update(config_utils.from_envvar_file('WEBANT_SETTINGS'))
-    app.config.update(config_utils.from_envvars(prefix='WEBANT_'))
+    app.config.update(conf)
     Bootstrap(app)
     babel = Babel(app)
     app.register_blueprint(agherant.agherant, url_prefix='/agherant')

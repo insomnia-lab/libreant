insomnia-lab/libreant

commit bed8a303f03ee0464c4d336a023e6a6fcb5e36ab

FIX agherant logging
closes #88

webant/agherant.py
@@ -40,6 +40,7 @@ def aggregate(descriptions, query):
     descriptions = map(autoreplace, descriptions)
     clients = []
     for url in descriptions:
+        log.debug("Fetching description %s")
         try:
             clients.append(Client(url))
         except:

webant/agherant_standalone.py
@@ -1,21 +1,24 @@
+import logging
+
 from flask import Flask, request
 from flask_bootstrap import Bootstrap
 from flask.ext.babel import Babel
 
 import agherant
+from webant import initLoggers
 from webserver_utils import gevent_run
 import config_utils
 
 
 def create_app():
+    initLoggers()
+    conf = {'DEBUG': True,
+            'AGHERANT_DESCRIPTIONS': []}
+    conf.update(config_utils.from_envvar_file('WEBANT_SETTINGS'))
+    conf.update(config_utils.from_envvars(prefix='WEBANT_'))
+    initLoggers(logging.DEBUG if conf.get('DEBUG', False) else logging.WARNING)
     app = Flask(__name__)
-    app.config.update({
-        'AGHERANT_DESCRIPTIONS': [],
-        'DEBUG': True,
-        'SECRET_KEY': 'really insecure, please change me!'
-    })
-    app.config.update(config_utils.from_envvar_file('WEBANT_SETTINGS'))
-    app.config.update(config_utils.from_envvars(prefix='WEBANT_'))
+    app.config.update(conf)
     Bootstrap(app)
     babel = Babel(app)
     app.register_blueprint(agherant.agherant, url_prefix='/agherant')

commit 74ef6e7b31a0afcd458f32932de0793bd1d1b9c6

minor fix: missing logging parameter
small logging bug introduced in bed8a303f03

webant/agherant.py
@@ -47,7 +47,7 @@ def aggregate(descriptions, query):
                           map(autoreplace, descriptions))
     clients = []
     for url in descriptions:
-        log.debug("Fetching description %s")
+        log.debug("Fetching description {}".format(url))
         try:
             clients.append(Client(url))
         except:
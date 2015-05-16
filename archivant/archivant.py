c22b02c9e0d87ff9fd8b691ca3f95c7a18efa2bb
ael-code <tommy.ael@gmail.com>
2015-05-16 11:06:03 -0500

    [archivant] added insert function into archivant
    file info structure has changed a little bit:
        We do not use `fsdb_id` anymore.
        Now we have `url` to locate file, this is much more flexible.
        In case of a file stored on local fsdb instance the 'url' value will be something like ```fsdb:///file-sha1```

--- archivant/archivant.py
+++ archivant/archivant.py
@@ -1,6 +1,11 @@
-from fsdb import Fsdb
-from libreantdb import DB
+import os
 from elasticsearch import Elasticsearch
+from uuid import uuid4
+from fsdb import Fsdb
+from fsdb.hashtools import calc_file_digest, calc_digest
+from copy import deepcopy
+
+from libreantdb import DB
 
 from logging import getLogger
 log = getLogger('archivant')
@@ -39,3 +44,86 @@ class Archivant():
                 index_name=self._config['ES_INDEXNAME'])
         db.setup_db()
         self._db = db
+
+    def insert_volume(self, metadata, attachments=[]):
+        '''Insert a new volume
+
+           Returns the ID of the added volume
+
+           `metadata` must be a dict containg metadata of the volume.
+           The only required key is `_language`
+           {
+             "_language" : "it",  # language of the metadata
+             "key1" : "value1",   # attribute
+             "key2" : "value2",
+              ...
+             "keyN" : "valueN"
+           }
+
+           `attachments` must be an array of dict
+           {
+               "file"  : "/prova/una/path/a/caso" # path or fp
+               "name"  : "nome_buffo.ext"         # name of the file (extension included) [optional if a path was given]
+               "mime"  : "application/json"       # mime type of the file [optional]
+               "notes" : "this file is awesome"   # notes that will be attached to this file [optional]
+           }
+        '''
+
+        log.debug("adding new volume:\n\tdata: {}\n\tfiles: {}".format(metadata, attachments))
+
+        requiredFields = ['_language']
+        for requiredField in requiredFields:
+            if requiredField not in metadata:
+                raise KeyError("Required field '{}' is missing".format(requiredField))
+
+        volume = deepcopy(metadata)
+
+        attsData = []
+        for index, a in enumerate(attachments):
+            attData = {}
+            # file is not optional
+            if 'file' not in a:
+                raise KeyError("`file` key not found in attachments array at index {}".format(index))
+
+            locator = a['file']
+            if isinstance(locator, basestring):
+                if not os.path.isfile(locator):
+                    raise ValueError("'{}' is not a regular file. attachments array at index {}".format(locator, index))
+                attData['name'] = a['name'] if 'name' in a else os.path.basename(locator)
+                attData['size'] = os.path.getsize(locator)
+                attData['sha1'] = calc_file_digest(locator, algorithm="sha1")
+
+            elif hasattr(locator, 'read') and hasattr(locator, 'seek'):
+                if 'name' in a and a['name']:
+                    attData['name'] = a['name']
+                elif hasattr(locator, 'name'):
+                    attData['name'] = locator.name
+                else:
+                    raise ValueError("Could not assign a name to the file. attachments array at index {}".format(index))
+
+                old_position = locator.tell()
+
+                locator.seek(0, os.SEEK_END)
+                attData['size'] = locator.tell() - old_position
+                locator.seek(old_position, os.SEEK_SET)
+
+                attData['sha1'] = calc_digest(locator, algorithm="sha1")
+                locator.seek(old_position, os.SEEK_SET)
+
+            else:
+                raise ValueError("unsupported file value type {} in attachments array at index {}".format(type(locator), index))
+
+            attData['id'] = uuid4().hex
+            attData['mime'] = a['mime'] if 'mime' in a else None
+            attData['notes'] = a['notes'] if 'notes' in a else ""
+            attData['download_count'] = 0
+            fsdb_id = self._fsdb.add(locator)
+            attData['url'] = "fsdb:///" + fsdb_id
+            attsData.append(attData)
+
+        volume['_attachments'] = attsData
+
+        log.debug('constructed volume for insertion: {}'.format(volume))
+        addedVolume = self._db.add_book(body=volume)
+        log.debug("added new volume: '{}'".format(addedVolume['_id']))
+        return addedVolume['_id']

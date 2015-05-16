c6958322c26747cc5722247f1d464621e87e98cb
ael-code <tommy.ael@gmail.com>
2015-05-16 12:16:39 -0500

    [archivant] added get_volume function

--- archivant/archivant.py
+++ archivant/archivant.py
@@ -1,11 +1,13 @@
 import os
 from elasticsearch import Elasticsearch
+from elasticsearch import NotFoundError
 from uuid import uuid4
 from fsdb import Fsdb
 from fsdb.hashtools import calc_file_digest, calc_digest
 from copy import deepcopy
 
 from libreantdb import DB
+from exceptions import NotFoundException
 
 from logging import getLogger
 log = getLogger('archivant')
@@ -44,6 +46,71 @@ class Archivant():
                 index_name=self._config['ES_INDEXNAME'])
         db.setup_db()
         self._db = db
+
+    @staticmethod
+    def normalize_volume(volume):
+        '''convert volume metadata from es to archivant format
+
+           output example:
+           {
+            'id': 'AU0paPZOMZchuDv1iDv8',
+            'type': 'volume',
+            'metadata': {'_language': 'en',
+                         'key1': 'value1',
+                         'key2': 'value2',
+                         'key3': 'value3'},
+            'attachments': [{'id': 'a910e1kjdo2d192d1dko1p2kd1209d',
+                             'type' : 'attachment',
+                             'url': 'fsdb:///624bffa8a6f90813b7982d0e5b4c1475ebec40e3',
+                             'metadata': {'download_count': 0,
+                                          'mime': 'application/json',
+                                          'name': 'tmp9fyat_',
+                                          'notes': 'this file is awsome',
+                                          'sha1': '624bffa8a6f90813b7982d0e5b4c1475ebec40e3',
+                                          'size': 10}
+                           }]
+           }
+        '''
+        res = dict()
+        res['type'] = 'volume'
+        res['id'] = volume['_id']
+        if '_score' in volume:
+            res['score'] = volume['score']
+
+        source = volume['_source']
+        attachments = source['_attachments']
+        del(source['_attachments'])
+        del(source['_text_' + source['_language']])
+        res['metadata'] = source
+
+        atts = list()
+        for attachment in attachments:
+            atts.append(Archivant.normalize_attachment(attachment))
+        res['attachments'] = atts
+
+        return res
+
+    @staticmethod
+    def normalize_attachment(attachment):
+        ''' Convert attachment metadata from es to archivant format '''
+        res = dict()
+        res['type'] = 'attachment'
+        res['id'] = attachment['id']
+        del(attachment['id'])
+        res['url'] = attachment['url']
+        del(attachment['url'])
+        res['metadata'] = attachment
+        return res
+
+    def _req_raw_volume(self, volumeID):
+        try:
+            return self._db.get_book_by_id(volumeID)
+        except NotFoundError:
+            raise NotFoundException("could not found volume with id: '{}'".format(volumeID))
+
+    def get_volume(self, volumeID):
+        log.debug("Requested volume with id:'{}'".format(volumeID))
+        return Archivant.normalize_volume(self._req_raw_volume(volumeID))
 
     def insert_volume(self, metadata, attachments=[]):
         '''Insert a new volume

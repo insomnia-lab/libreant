bfbff72f4fa67500e51fbbaa173bab0c6b0105ae
ael-code <tommy.ael@gmail.com>
2015-05-25 07:03:08 -0500

    [libreantdb] cleanup update_book tests

--- libreantdb/test/test_update.py
+++ libreantdb/test/test_update.py
@@ -11,32 +11,21 @@ from . import db, cleanall
 @with_setup(cleanall, cleanall)
 def test_update_book_simple():
     '''Update a book, no merging'''
-    id_ = db.add_book(doc_type='book',
-                      body=dict(title='La fine', _language='it'))['_id']
+    id_ = db.add_book(body=dict(title='La fine', _language='it'))['_id']
     db.es.indices.refresh(index=db.index_name)
-    res = db.get_books_simplequery('fine')
-    eq_(res['hits']['total'], 1)
-
-    db.update_book(id_, doc_type='book',
-                   body=dict(title='Ciao mondo', _language='it'))
+    db.update_book(id_, body=dict(title='Ciao mondo'))
     db.es.indices.refresh(index=db.index_name)
     res = db.get_books_simplequery('fine')
     eq_(res['hits']['total'], 0)
     res = db.get_books_simplequery('mondo')
     eq_(res['hits']['total'], 1)
 
-
 @with_setup(cleanall, cleanall)
 def test_update_book_merge():
     '''Update a book, merging'''
-    id_ = db.add_book(doc_type='book',
-                      body=dict(title='La fine', _language='it'))['_id']
+    id_ = db.add_book(body=dict(title='La fine', _language='it'))['_id']
     db.es.indices.refresh(index=db.index_name)
-    res = db.get_books_simplequery('fine')
-    eq_(res['hits']['total'], 1)
-
-    db.update_book(id_, doc_type='book',
-                   body=dict(foo='Ciao mondo', _language='it'))
+    db.update_book(id_, body=dict(foo='Ciao mondo'))
     db.es.indices.refresh(index=db.index_name)
     res = db.get_books_simplequery('fine')
     eq_(res['hits']['total'], 1)
@@ -47,11 +36,9 @@ def test_update_book_merge():
 @with_setup(cleanall, cleanall)
 def test_update_book_merge_fts_correct():
     '''Update a book, merging. FTS fields should still be correct'''
-    id_ = db.add_book(doc_type='book',
-                      body=dict(title='La fine', _language='it'))['_id']
+    id_ = db.add_book(body=dict(title='La fine', _language='it'))['_id']
     db.es.indices.refresh(index=db.index_name)
-    db.update_book(id_, doc_type='book',
-                   body=dict(foo='Ciao mondo', _language='it'))
+    db.update_book(id_, body=dict(foo='Ciao mondo'))
     db.es.indices.refresh(index=db.index_name)
     res = db.get_books_simplequery('mondo')
     src = res['hits']['hits'][0]['_source']

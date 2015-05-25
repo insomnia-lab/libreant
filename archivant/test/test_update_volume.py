b040eda2b66a5015816391a9e5c4221e031de9b8
ael-code <tommy.ael@gmail.com>
2015-05-25 18:04:33 -0500

    [archivant] added tests for update_volume

--- archivant/test/test_update_volume.py
+++ archivant/test/test_update_volume.py
@@ -20,3 +20,40 @@ class TestArchivantUpdateVolume(TestArchivant):
         del(rawVolume['_source']['_text_'+rawVolume['_source']['_language']])
         eq_(denom_id, id)
         eq_(denormalized, rawVolume['_source'])
+
+    def test_update_volume_add(self):
+        volume_metadata = {'_language': 'en',
+                           'old_1': 'old_value_1'}
+        id = self.arc.insert_volume(volume_metadata)
+        from pprint import pprint
+        pprint(volume_metadata)
+        volume_metadata['new_1'] = 'new_value_1'
+        self.arc.update_volume(id, volume_metadata)
+        vol_metadata = (self.arc.get_volume(id))['metadata']
+        eq_(vol_metadata['_language'], 'en')
+        eq_(vol_metadata['old_1'], 'old_value_1')
+        eq_(vol_metadata['new_1'], 'new_value_1')
+
+    def test_update_volume_change(self):
+        volume_metadata = {'_language': 'en',
+                           'old_1': 'old_value_1',
+                           'old_2': 'old_value_2'}
+        id = self.arc.insert_volume(volume_metadata)
+        volume_metadata['old_2'] = 'new_value_2'
+        volume_metadata['_language'] = 'it'
+        self.arc.update_volume(id, volume_metadata)
+        vol_metadata = (self.arc.get_volume(id))['metadata']
+        eq_(vol_metadata['_language'], 'it')
+        eq_(vol_metadata['old_1'], 'old_value_1')
+        eq_(vol_metadata['old_2'], 'new_value_2')
+
+    def test_update_volume_delete(self):
+        volume_metadata = {'_language': 'en',
+                           'old_1': 'old_value_1',
+                           'old_2': 'old_value_2'}
+        id = self.arc.insert_volume(volume_metadata)
+        del(volume_metadata['old_2'])
+        self.arc.update_volume(id, volume_metadata)
+        vol_metadata = (self.arc.get_volume(id))['metadata']
+        eq_(vol_metadata['_language'], 'en')
+        eq_(vol_metadata['old_1'], 'old_value_1')

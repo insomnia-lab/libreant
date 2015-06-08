68be3ba181e2cd92d0266df286a3d6e787f090cf
ael-code <tommy.ael@gmail.com>
2015-06-08 09:08:50 -0500

    setup.py pep8 adherence

--- setup.py
+++ setup.py
@@ -27,17 +27,17 @@ class compile_translations(Command):
            Skips not changed file based on source mtime.
         """
         # thanks to deluge guys ;)
-        po_dir = os.path.join(os.path.dirname(__file__), 'webant','translations')
+        po_dir = os.path.join(os.path.dirname(__file__), 'webant', 'translations')
         print('Compiling po files from "{}"...'.format(po_dir))
         for lang in os.listdir(po_dir):
             sys.stdout.write("\tCompiling {}... ".format(lang))
             sys.stdout.flush()
-            curr_lang_path = os.path.join(po_dir,lang)
+            curr_lang_path = os.path.join(po_dir, lang)
             for path, dirs, filenames in os.walk(curr_lang_path):
                 for f in filenames:
                     if f.endswith('.po'):
                         src = os.path.join(path, f)
-                        dst = os.path.join(path, f[:-3]+".mo")
+                        dst = os.path.join(path, f[:-3] + ".mo")
                         if not os.path.exists(dst) or self.force:
                             msgfmt.make(src, dst)
                             print("ok.")
@@ -53,18 +53,18 @@ class compile_translations(Command):
 
 
 class build(_build):
-    sub_commands =  [('compile_translations', None)] + _build.sub_commands
+    sub_commands = [('compile_translations', None)] + _build.sub_commands
 
 
 class install_lib(_install_lib):
     def run(self):
-        self.run_command('compile_translations' )
+        self.run_command('compile_translations')
         _install_lib.run(self)
 
 
 class develop(_develop):
-     def run(self):
-        self.run_command('compile_translations' )
+    def run(self):
+        self.run_command('compile_translations')
         _develop.run(self)
 
 
@@ -104,28 +104,27 @@ setup(name='libreant',
           'Fsdb'
       ],
       package_data = {
-            # If any package contains *.mo include them:
-            # important! leave all the stars!
-            'webant': ['translations/*/*/*.mo']
-        },
+          # If any package contains *.mo include them
+          # important! leave all the stars!
+          'webant': ['translations/*/*/*.mo']
+      },
       include_package_data=True,
       tests_require=['nose', 'coverage'],
       zip_safe=False,
       cmdclass={'build': build,
                 'test': NoseTestCommand,
                 'install_lib': install_lib,
-                'develop':develop,
-                'compile_translations': compile_translations },
+                'develop': develop,
+                'compile_translations': compile_translations},
       entry_points={'console_scripts': [
           'webant=webant.webant:main',
           'libreant-manage=webant.manage:main',
           'agherant=webant.agherant_standalone:main'
-      ] },
+      ]},
       classifiers=[
-                  "Framework :: Flask",
-                  "License :: OSI Approved :: GNU Affero General Public License v3",
-                  "Operating System :: POSIX :: Linux",
-                  "Programming Language :: Python :: 2",
-                  "Development Status :: 4 - Beta"
-                  ]
-      )
+          "Framework :: Flask",
+          "License :: OSI Approved :: GNU Affero General Public License v3",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python :: 2",
+          "Development Status :: 4 - Beta"
+      ])

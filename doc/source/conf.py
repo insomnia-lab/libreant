78d522141573d89f0ff24b516364f8ae83024e28
ael-code <tommy.ael@gmail.com>
2015-06-27 12:37:01 -0500

    [docs] add package root to sys.path
    fixes #173

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,12 +12,13 @@
 
 import sys
 import os
-from setup import conf
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
+
+from setup import conf
 
 # -- General configuration ------------------------------------------------
 

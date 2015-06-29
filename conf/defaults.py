a87ff79a4eaaf51a9884212fb45366b09cb7826d
ael-code <tommy.ael@gmail.com>
2015-06-29 08:29:56 -0500

    [conf] added new defaults module

---
+++ conf/defaults.py
@@ -0,0 +1,25 @@
+defConf = {
+  'DEBUG':        (False, "operate in debug mode"),
+  'PORT':         (5000, "port on which daemon will listen"),
+  'ADDRESS':      ("0.0.0.0", "address on which daemon will listen"),
+  'FSDB_PATH':    (None, "path used for storing binary files"),
+  'ES_INDEXNAME': ('libreant', "index name to use for elasticsearch"),
+  'ES_HOSTS':     (None, "list of elasticsearch nodes to connect to"),
+  'PRESET_PATHS': ([], "list of paths where to look for presets definition"),
+  'AGHERANT_DESCRIPTIONS': (None, "list of description urls of nodes to aggregate"),
+  'BOOTSTRAP_SERVE_LOCAL': (True, "decide to serve bootstrap related files as local content"),
+  'MAX_RESULTS_PER_PAGE': (50, "number of max results for one request")
+}
+
+
+def get_def_conf():
+    '''return default configurations as simple dict'''
+    ret = dict()
+    for k,v in defConf.items():
+        ret[k] = v[0]
+    return ret
+
+
+def get_help(conf):
+    '''return the help message of a specific configuration parameter'''
+    return defConf[conf][1]

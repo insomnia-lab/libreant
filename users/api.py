e11c0afbe95806f6ebe4edd991202349efe28d78
ael-code <tommy.ael@gmail.com>
2015-10-08 05:56:44 -0500

    added groups api

--- users/api.py
+++ users/api.py
@@ -1,4 +1,4 @@
-from users import User, db
+from users import User, Group, db
 from peewee import IntegrityError
 
 
@@ -34,6 +34,64 @@ def get_user(id=None, name=None):
             raise ValueError('nither `id` or `name` was given')
     except User.DoesNotExist:
         raise NotFoundException("no user could be found with these attributes")
+
+
+def add_group(name):
+    try:
+        return Group.create(name=name)
+    except IntegrityError:
+        raise ConflictException("a group with the same name already exists")
+
+
+def delete_group(id):
+    if not Group.delete().where(Group.id == id).execute():
+        raise NotFoundException('no group could be found with this id')
+
+
+def update_group(id, updates):
+    with db.atomic():
+        g = get_group(id)
+        if 'name' in updates:
+            g.name = updates['name']
+        g.save()
+
+
+def get_group(id=None, name=None):
+    try:
+        if id:
+            return Group.get(Group.id==id)
+        elif name:
+            return Group.get(Group.name==name)
+        else:
+            raise ValueError('nither `id` or `name` was given')
+    except Group.DoesNotExist:
+        raise NotFoundException("no group could be found with these attributes")
+
+
+def add_user_to_group(userID, groupID):
+    with db.atomic():
+        u = get_user(userID)
+        g = get_group(groupID)
+        u.groups.add(g)
+        u.save()
+
+
+def remove_user_from_group(userID, groupID):
+    with db.atomic():
+        u = get_user(userID)
+        g = get_group(groupID)
+        u.groups.remove(g)
+        u.save()
+
+
+def get_groups_of_user(userID):
+    for g in get_user(id=userID).groups:
+        yield g
+
+
+def get_users_in_group(groupID):
+    for u in get_group(id=groupID).users:
+        yield u
 
 
 class NotFoundException(Exception):
--- webant/api/users_api.py
+++ webant/api/users_api.py
@@ -58,3 +58,89 @@ def update_user(userID):
     except users.api.NotFoundException, e:
         raise ApiError("Not found", 404, details=str(e))
     return make_success_response("user has been successfully updated")
+
+
+@route('/groups/<int:groupID>', methods=['GET'])
+def get_group(groupID):
+    try:
+        g = users.api.get_group(id=groupID)
+    except users.api.NotFoundException, e:
+        raise ApiError("Not found", 404, details=str(e))
+    return jsonify({'data': {'id': g.id, 'name': g.name}})
+
+
+@route('/groups/<int:groupID>', methods=['DELETE'])
+def delete_group(groupID):
+    try:
+        users.api.delete_group(id=groupID)
+    except users.api.NotFoundException, e:
+        raise ApiError("Not found", 404, details=str(e))
+    return make_success_response("group has been successfully deleted")
+
+
+@route('/groups/', methods=['POST'])
+def add_group():
+    request.on_json_loading_failed = on_json_load_error
+    groupData = request.json
+    if not groupData:
+        raise ApiError("Unsupported media type", 415)
+    name = groupData.get('name', None)
+    if not name:
+        raise ApiError("Bad Request", 400, details="missing 'name' parameter")
+    try:
+        group = users.api.add_group(name=name)
+    except users.api.ConflictException, e:
+        raise ApiError("Conflict", 409, details=str(e))
+    link_self = url_for('.get_group', groupID=group.id, _external=True)
+    response = jsonify({'data': {'id': group.id, 'link_self': link_self}})
+    response.status_code = 201
+    response.headers['Location'] = link_self
+    return response
+
+
+@route('/groups/<int:groupID>', methods=['PATCH'])
+def update_group(groupID):
+    request.on_json_loading_failed = on_json_load_error
+    if not request.json:
+        raise ApiError("Unsupported media type", 415)
+    try:
+        users.api.update_group(groupID, request.json)
+    except users.api.NotFoundException, e:
+        raise ApiError("Not found", 404, details=str(e))
+    return make_success_response("group has been successfully updated")
+
+
+@route('/groups/<int:groupID>/users/<int:userID>', methods=['PUT'])
+def add_user_to_group(groupID, userID):
+    try:
+        users.api.add_user_to_group(userID, groupID)
+    except users.api.NotFoundException, e:
+        raise ApiError("Not found", 404, details=str(e))
+    return make_success_response("user has been successfully added to group")
+
+
+@route('/groups/<int:groupID>/users/<int:userID>', methods=['DELETE'])
+def delete_user_from_group(groupID, userID):
+    try:
+        users.api.remove_user_from_group(userID, groupID)
+    except users.api.NotFoundException, e:
+        raise ApiError("Not found", 404, details=str(e))
+    return make_success_response("user has been successfully removed from group")
+
+
+@route('/groups/<int:groupID>/users/', methods=['GET'])
+def get_users_in_group(groupID):
+    try:
+        us = [{'id': u.id} for u in users.api.get_users_in_group(groupID)]
+    except users.api.NotFoundException, e:
+        raise ApiError("Not found", 404, details=str(e))
+    return jsonify({'data': us})
+
+
+@route('/users/<int:userID>/groups/', methods=['GET'])
+def get_groups_of_user(userID):
+    try:
+        groups = [{'id': g.id} for g in users.api.get_groups_of_user(userID)]
+    except users.api.NotFoundException, e:
+        raise ApiError("Not found", 404, details=str(e))
+    return jsonify({'data': groups})

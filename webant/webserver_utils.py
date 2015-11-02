7d98781b97245a5dd4078074ed2fc7dae0367f90
boyska <piuttosto@logorroici.org>
2015-11-03 00:54:51 +0100

    http server reloads only when DEBUG=True
    fixes #204

--- webant/webserver_utils.py
+++ webant/webserver_utils.py
@@ -7,16 +7,20 @@ def gevent_run(app):
     from gevent.wsgi import WSGIServer
     import gevent.monkey
     from werkzeug.debug import DebuggedApplication
-    from werkzeug.serving import run_with_reloader
     gevent.monkey.patch_socket()
     run_app = app
     if app.config['DEBUG']:
         run_app = DebuggedApplication(app)
 
-    @run_with_reloader
     def run_server():
         port = int(app.config.get('PORT', 5000))
         address = app.config.get('ADDRESS', '')
         print('Listening on http://%s:%d/' % (address or '0.0.0.0', port))
         http_server = WSGIServer((address, port), run_app)
         http_server.serve_forever()
+
+    if app.config['DEBUG']:
+        from werkzeug.serving import run_with_reloader
+        run_with_reloader(run_server)
+    else:
+        run_server()

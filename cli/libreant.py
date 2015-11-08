94accd8a16e701e6d86b9c9c515f92517ec07a1a
ael-code <tommy.ael@gmail.com>
2015-11-08 06:10:06 -0600

    cli: expose users database configuration option
    make possible to use --users-db to configure users databse url.

--- cli/libreant.py
+++ cli/libreant.py
@@ -18,10 +18,11 @@ from custom_types import StringList
 @click.option('--fsdb-path', type=click.Path(), metavar="<path>", help=get_help('FSDB_PATH'))
 @click.option('--es-indexname', type=click.STRING, metavar="<name>", help=get_help('ES_INDEXNAME'))
 @click.option('--es-hosts', type=StringList(), metavar="<host>..", help=get_help('ES_HOSTS'))
+@click.option('--users-db', type=click.Path(), metavar="<url>", help=get_help('USERS_DATABASE') )
 @click.option('--preset-paths', type=StringList(), metavar="<path>..", help=get_help('PRESET_PATHS'))
 @click.option('--agherant-descriptions', type=StringList(), metavar="<url>..", help=get_help('AGHERANT_DESCRIPTIONS'))
 @click.option('--dump-settings', is_flag=True, help='dump current settings and exit')
-def libreant(settings, debug, port, address, fsdb_path, es_indexname, es_hosts, preset_paths, agherant_descriptions, dump_settings):
+def libreant(settings, debug, port, address, fsdb_path, es_indexname, es_hosts, users_db, preset_paths, agherant_descriptions, dump_settings):
     initLoggers(logNames=['config_utils'])
     conf = config_utils.load_configs('LIBREANT_', defaults=get_def_conf(), path=settings)
     cliConf = {}
@@ -37,6 +38,8 @@ def libreant(settings, debug, port, address, fsdb_path, es_indexname, es_hosts, 
         cliConf['ES_INDEXNAME'] = es_indexname
     if es_hosts:
         cliConf['ES_HOSTS'] = es_hosts
+    if users_db:
+        cliConf['USERS_DATABASE'] = users_db
     if preset_paths:
         cliConf['PRESET_PATHS'] = preset_paths
     if agherant_descriptions:

4ef7116cdf2878d63e746e3919390f7f6da0eda8
ael-code <tommy.ael@gmail.com>
2016-03-16 15:28:20 +0100

    bump to v0.4

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ def read(fname):
 
 conf = dict(
         name='libreant',
-        version='0.3',
+        version='0.4',
         description='{e,}book archive focused on small grass root archives, distributed search, low assumptions',
         long_description=read('README.rst'),
         author='insomnialab',

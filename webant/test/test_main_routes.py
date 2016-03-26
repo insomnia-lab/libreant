1c050a883be375c899a4de65770c33fce7d888c3
ael-code <tommy.ael@gmail.com>
2016-03-26 13:27:50 +0100

    webant test base class
    some tests were working on the default `libreant` index and they did not delete it after they've finished.
    
    Now the WebantTestCase class make use of a temporary index that will be deeleted at the end of the class level tests.
    
    Moreover now it's easier to subclass the WebantTestCase class.
    As you can see in the `WebantUsersTestCase` class it is possible to use the `setUpClass` method to override the webant configuration that are going to be use during the tests.

--- webant/test/test_main_routes.py
+++ webant/test/test_main_routes.py
@@ -1,32 +1,20 @@
-from webant import create_app
-from conf.defaults import get_def_conf
+from . import WebantTestCase
 from nose.tools import eq_
 
 
-def get_wtc():
-    return create_app(get_def_conf()).test_client()
+class WebantMainRoutes(WebantTestCase):
 
+    def test_home(self):
+        eq_(self.wtc.get('/').status_code, 200)
 
-def test_home():
-    wtc = get_wtc()
-    eq_(wtc.get('/').status_code, 200)
+    def test_get_add(self):
+        eq_(self.wtc.get('/add').status_code, 200)
 
+    def test_empty_search(self):
+        eq_(self.wtc.get('/search?q=*').status_code, 200)
 
-def test_get_add():
-    wtc = get_wtc()
-    eq_(wtc.get('/add').status_code, 200)
+    def test_descr(self):
+        eq_(self.wtc.get('/description.xml').status_code, 200)
 
-
-def test_empty_search():
-    wtc = get_wtc()
-    eq_(wtc.get('/search?q=*').status_code, 200)
-
-
-def test_descr():
-    wtc = get_wtc()
-    eq_(wtc.get('/description.xml').status_code, 200)
-
-
-def test_recents():
-    wtc = get_wtc()
-    eq_(wtc.get('/recents').status_code, 200)
+    def test_recents(self):
+        eq_(self.wtc.get('/recents').status_code, 200)

200d7af55ea3044d0a089bbc643c59d5d69d0ab7
ael-code <tommy.ael@gmail.com>
2016-05-01 17:13:52 +0200

    remove score from get_all_volumes results

--- archivant/archivant.py
+++ archivant/archivant.py
@@ -172,7 +172,9 @@ class Archivant():
     def get_all_volumes(self):
         '''iterate over all stored volumes'''
         for raw_volume in self._db.iterate_all():
-            yield self.normalize_volume(raw_volume)
+            v = self.normalize_volume(raw_volume)
+            del v['score']
+            yield v
 
     def get_volume(self, volumeID):
         log.debug("Requested volume with id:'{}'".format(volumeID))

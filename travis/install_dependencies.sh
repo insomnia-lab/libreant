28c20ee3d1726df9be9cd961560209f1574e4b04
boyska <piuttosto@logorroici.org>
2016-10-31 13:25:56 +0100

    FIX 2.x on script

--- travis/install_dependencies.sh
+++ travis/install_dependencies.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 if [[ $VERSION_ES == "2.x" ]]; then
-    sudo apt-get autoremove --purge elasticsearch
-    wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-    echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elk.list
-    sudo apt-get update && sudo apt-get install elasticsearch -y
-    sudo service elasticsearch start
-    sed -i "s/\('elasticsearch\)\(.*\)\('\)/\1 >2\3/g" setup.py
-    pip install --upgrade elasticsearch>=2.0
+    sed -i "s/\('elasticsearch\)\(.*\)\('\)/\1 >=2.0.0, <3.0.0\3/" setup.py
+    pip install .
 fi
+
+echo -e "travis_fold:start:ESinfo\r"
+dpkg-query -W -f='DEB ${Package} ${Version}\n' elasticsearch
+pip freeze|grep elasticsearch
+echo -e "travis_fold:end:ESinfo\r"
